a36dd1ec022baf30f8c3dfe0235f407ea2a7b631
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-08-26 22:22:51 -0300

    feat: agregar template de la app

---
+++ app/src/main/java/ar/edu/davinci/a252_am_lessons/MainActivity.java
@@ -0,0 +1,15 @@
+package ar.edu.davinci.a252_am_lessons;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("testing", "Probando mensajes");
+    }
+}
