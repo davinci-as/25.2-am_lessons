cc39b1001a786b4c34076bc531f3f1e9beafb2ae
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-09-09 22:11:11 -0300

    feat: agregar vistas dinamicamente

--- app/src/main/java/ar/edu/davinci/a252_am_lessons/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/a252_am_lessons/MainActivity.java
@@ -3,6 +3,9 @@ package ar.edu.davinci.a252_am_lessons;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 1. Encontrar container
+        LinearLayout container = findViewById(R.id.container);
+
+        // 2. Crear el textview
+        TextView textView = new TextView(this);
+        textView.setText(R.string.welcome_message);
+
+        // 3. Crear el button
+        Button button = new Button(this);
+        button.setText(R.string.button_message);
+
+        // 4. Agregar los hijos al container
+        container.addView(textView);
+        container.addView(button);
+
         Log.i("testing", String.valueOf(R.string.log_message));
     }
 }
