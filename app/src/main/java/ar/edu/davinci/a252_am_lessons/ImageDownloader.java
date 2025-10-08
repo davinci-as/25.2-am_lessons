42a7fdcd5bad3875236c81c940e8aa12cfbc5b98
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-10-07 22:38:36 -0300

    feat: descargar imagen usando inputstream

--- app/src/main/java/ar/edu/davinci/a252_am_lessons/ImageDownloader.java
+++ app/src/main/java/ar/edu/davinci/a252_am_lessons/ImageDownloader.java
@@ -1,13 +1,25 @@
 package ar.edu.davinci.a252_am_lessons;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ImageDownloader extends AsyncTask<String, Integer, Bitmap> {
     @Override
     protected Bitmap doInBackground(String... strings) {
         String content = strings[0];
-        return null;
+        try {
+            URL url_origen = new URL(content);
+            InputStream input = (InputStream) url_origen.getContent();
+            return BitmapFactory.decodeStream(input);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
