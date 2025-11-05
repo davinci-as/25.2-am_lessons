0a25f429175efb95cd02edf562323800f9cabc2c
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2025-11-04 22:58:03 -0300

    feat: add firestore setup

--- app/src/main/java/ar/edu/davinci/a252_am_lessons/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/a252_am_lessons/MainActivity.java
@@ -14,23 +14,47 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
         FirebaseUser user = mAuth.getCurrentUser();
 
         if(user != null) {
             //TODO: eliminar después de la prueba
             //mAuth.signOut();
+            String UID = user.getUid();
+            db.collection("users")
+                    .whereEqualTo("uid", UID)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Log.d("firebase-firestore", document.getId() + " => " + document.getData().get("name"));
+                                }
+                            } else {
+                                Log.d("firebase-firestore", "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
         } else {
            Intent intent = new Intent(this, LoginActivity.class);
            startActivity(intent);
